a0c7f98ce18b31dc60925eaa51878f65a646db07
Giovana Santana Barbosa <giovanastbarbosa@gmail.com>
2023-06-04 15:44:52 -0300

    Footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import './footer.css';
+
+function Footer(){
+    return(
+        <footer>
+            <span>Site desenvolvido por <a href="https://github.com/giovanasbarbosa" target="blank">Giovana Barbosa</a></span>
+        </footer>
+    )
+}
+
+export default Footer;
