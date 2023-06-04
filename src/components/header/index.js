2e81fb75d1261e41681ee627d5a4b7f7eb7bafe9
Giovana Santana Barbosa <giovanastbarbosa@gmail.com>
2023-06-04 12:27:05 -0300

    Alterações do homepage

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 function Header(){
     return(
         <header>
-            <Link className='logo' to='/'>PrimeFlix</Link>
+            <Link className='logo' to='/React-PrimeFlix'>PrimeFlix</Link>
             <Link className='favoritos' to='/favoritos'>Meus filmes</Link>
         </header>
     )
